f4817e9c2de7987e0599a66cf564fa8c0dc4e6ba
Deo Seth <diothewind@gmail.com>
2024-04-23 14:27:26 +0800

    Expand statistics section

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,31 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [score, addScore] = useState([])
 
-  const addVoteToGood = () => setGood(good + 1)
-  const addVoteToNeutral = () => setNeutral(neutral + 1)
-  const addVoteToBad = () => setBad(bad + 1)
+  const totalVotes = good + neutral + bad
+  const totalScore = score.reduce((previousScore, currentScore) => {
+    return previousScore + currentScore
+  }, 0)
+  const positiveFeedback = (good / totalVotes) * 100
+
+  const addVoteToGood = () => {
+    setGood(good + 1)
+    const updatedScore = score.concat(1)
+    addScore(updatedScore)
+  }
+
+  const addVoteToNeutral = () => {
+    setNeutral(neutral + 1)
+    const updatedScore = score.concat(0)
+    addScore(updatedScore)
+  }
+
+  const addVoteToBad = () => {
+    setBad(bad + 1)
+    const updatedScore = score.concat(-1)
+    addScore(updatedScore)
+  }
 
   return (
     <div>
@@ -22,6 +43,9 @@ const App = () => {
         <p>good: {good}</p>
         <p>neutral: {neutral}</p>
         <p>bad: {bad}</p>
+        <p>all: {totalVotes}</p>
+        <p>average: {totalScore / totalVotes}</p>
+        <p>positive: {positiveFeedback}%</p>
       </div>
     </div>
   )
